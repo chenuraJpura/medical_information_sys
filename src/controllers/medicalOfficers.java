34a639c39fcc71fba62629c60b29792a5ebfa8da
Your Name <monalitaniya17@gmail.com>
2020-12-07 12:06:30 +0530

    #2 medicalOfficers and appointmentController classes updated.

--- src/controllers/medicalOfficers.java
+++ src/controllers/medicalOfficers.java
@@ -1,11 +1,19 @@
+/*
+Name       : medical information system (Team 09)
+version    : 2.0
+Title      : medicalOfficers.java
+description: handle the medicalOfficers subclass
 
+*/
 package controllers;
 
-public class medicalOfficers extends userController{
+public class medicalOfficers extends userController{//start the medical officers subclass inherit from the user controller parentclass
+    // declare the private variables
     private int medicalStaffID;
     private String medicalStaffEmail;
     private String medicalSpecializedArea;
-
+    
+    //default constructor
     public medicalOfficers(int medicalStaffID,
             String medicalStaffEmail,
             String medicalSpecializedArea,
@@ -18,6 +26,7 @@ public class medicalOfficers extends userController{
             String userAddress,
             String userGender,
             String mariedState) {
+        //here super keyword is used to call methods and access the constructors in the user controller supper class.
         super(userName, userFirstName,
                 userLastName,
                 userPhoneNo,
@@ -26,16 +35,16 @@ public class medicalOfficers extends userController{
                 userAddress,
                 userGender,
                 mariedState);
-        this.medicalStaffID = medicalStaffID;
-        this.medicalStaffEmail = medicalStaffEmail;
-        this.medicalSpecializedArea=medicalSpecializedArea;
+        this.setMedicalStaffID (medicalStaffID);
+        this.setMedicalStaffEmail (medicalStaffEmail);
+        this.setMedicalSpecializedArea(medicalSpecializedArea);// end of the default constructor
     }
     @Override
-    public String toString() {
+    public String toString() { //here return the values by toString method
         return getMedicalStaffID()+","+
                getMedicalStaffEmail()+","+
                getMedicalSpecializedArea()+","+
-               super.getUserName()+","+
+               super.getUserName()+","+    //call the methods from the user controller supper class
                super.getUserFirstName()+","+
                super.getUserLastName()+","+
                super.getUserPhoneNo()+","+
@@ -45,7 +54,7 @@ public class medicalOfficers extends userController{
                super.getUserGen()+","+
                super.getUserMarried();
     }
-
+    //create the getters and setters for variables
     public String getMedicalSpecializedArea() {
         return medicalSpecializedArea;
     }
@@ -70,9 +79,9 @@ public class medicalOfficers extends userController{
 
     public void setMedicalStaffEmail(String medicalStaffEmail) {
         this.medicalStaffEmail = medicalStaffEmail;
-    }
+    }// end of the getters and setters
     
     
             
     
-}
+}//end of the medical officer subclass
