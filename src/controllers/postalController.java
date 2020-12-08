ec1e84fca800784e070e28eebdfb6edd6b669e91
monalitaniya <74762750+monalitaniya@users.noreply.github.com>
2020-12-08 20:41:43 +0530

    #3 complaisController and postalController classes updated. (#10)

--- src/controllers/postalController.java
+++ src/controllers/postalController.java
@@ -1,18 +1,26 @@
+/*
+Name       : medical information system (Team 09)
+version    : 2.0
+Title      : postalController.java
+Description: handle the postalController class
+*/
 package controllers;
 
-public class postalController {
+public class postalController {// start of the postalController class
+    // declare the private variables
     private int postRefferenceNo;
     private String postNote;
     private date postDateObj;
-
+            
+    //create default constructors
     public postalController(int postRefferenceNo, String postNote, date postDateObj) {
-        this.postRefferenceNo = postRefferenceNo;
-        this.postNote = postNote;
-        this.postDateObj = postDateObj;
+        this.setPostRefferenceNo (postRefferenceNo);
+        this.setPostNote (postNote);
+        this.setPostDateObj (postDateObj);
     }
 
     
-    
+    // start getters and setters for variables
     public int getPostRefferenceNo() {
         return postRefferenceNo;
     }
@@ -35,7 +43,7 @@ public class postalController {
 
     public void setPostDateObj(date postDateObj) {
         this.postDateObj = postDateObj;
-    }
+    }// end of the getters and setters
     
     
-}
+}// end of the postalController class
