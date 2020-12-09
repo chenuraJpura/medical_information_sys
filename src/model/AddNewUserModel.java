ba7987af7cd68302473a072152b737161d8f661b
Your Name <monalitaniya17@gmail.com>
2020-12-09 15:02:43 +0530

    #4 AddNewPatientModel and AddNewUserModel updated

--- src/model/AddNewUserModel.java
+++ src/model/AddNewUserModel.java
@@ -1,11 +1,18 @@
-package model;
+/*
+Name       : medical information system (Team 09)
+version    : 2.0
+Title      : AddNewUserModel.java
+Description: Writting text files and streams for a new user
+*/
+package model;//default package
 
+//import the external pluggings
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class AddNewUserModel {
+public class AddNewUserModel {// start of the AddNewUserModel class
     public static void writingTXT(String AddNewUserDetail){//writing to objcet to file 
     BufferedWriter bw = null;
       try {//try catch start
@@ -18,31 +25,31 @@ public class AddNewUserModel {
                     
                 }
                 
-                FileWriter fw = new FileWriter(file,true);
+                FileWriter fw = new FileWriter(file,true);//open the file for writing
                 
                 bw = new BufferedWriter(fw);
                 
-                bw.write(AddNewUserDetail);
-                bw.newLine();
+                bw.write(AddNewUserDetail);// new user details wtiting to the line
+                bw.newLine();//create a new line
         
 
-                    
+        // catch error of writting or closing the file               
       } catch (IOException ioe) {
           System.out.println(ioe);
           ioe.printStackTrace();
       }
 	finally
     { 
-        try{
+        try{// start try catch
                 if(bw!=null){
-                        bw.close();
+                        bw.close();//closinf the buffer reader
                             }
-                    }catch(Exception ex)
+                    }catch(Exception ex)// catch
                     
                     {
-                            System.out.println("Error in closing the BufferedWriter"+ex);
+                            System.out.println("Error in closing the BufferedWriter"+ex);//display the error
            }
 	}
     }//end of the function
     
-}
+}// end of the AddNewUserModel class
